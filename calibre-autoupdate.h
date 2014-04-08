e87cdccaaa36b02ec50d511b9ffeda018135ea55
Leon Gaultier <leongaultier@gmail.com>
2014-04-08 06:49:28 +0200

    sauberer Code an einigen stellen, die Dialoge verbessert, Ergebnisse von Funktionsaufrufen in Variablen geschrieben

--- calibre-autoupdate.h
+++ calibre-autoupdate.h
@@ -15,16 +15,19 @@ func_help ()
     echo "$PROGNAME -- Der Calibre! Linux Edition calibre-autoupdater (CAU)"
     echo ""
     echo "Options:"
-    echo "   -q, --quiet           ohne Konsolenausgabe"
-    echo "   -h, --help            zeigt die Hilfe an"
-    echo "   -V, --version         gibt die Programmversion aus"
-    echo "   -c, --check           kontrolliert ob benötigte Programme vorhanden sind"
+    echo "   -q, --quiet                  ohne Konsolenausgabe"
+    echo "   -h, --help                   zeigt die Hilfe an"
+    echo "   -V, --version                gibt die Programmversion aus"
+    echo "   -c, --check                  kontrolliert ob benötigte Programme vorhanden sind"
+    echo "   -u, --uninstall              deinstalliert den Calibre-Autoupdater"
+    echo "       --uninstall_calibre      deinstalliert das Calibre Programm"
     echo ""
     echo -e "Sende Fehler an <leongaultier@gmail.com>.\n";
 }
 
 func_version ()
 {
+    func_term_output
     echo "$PROGNAME: Der Calibre! autoupdater v$VERSION"
     echo "Leon Gaultier 2013-2014. Das Programm darf verteilt werden,"
     echo "in der Hoffnung das es hilfreich ist, aber OHNR IRGENDEINE GARANTIE"
@@ -62,6 +65,13 @@ func_vercomp () {            # Funktion zum Versionsvergleich
     return 0
 }
 
+func_term_output () {
+  ## Beginne mit der Ausgbe ##
+  tty -s && clear
+  echo -e "\033[44m \033[0m\033[1m $PROGNAME Version $VERSION\033[0m\n\033[44m \033[0m 2013-2014 by Leon Gaultier\n\e[m";
+  return 0
+}
+
 func_progressbar()
 {
     echo -e -n $'\033[34m------------------------------------------------------------------------- 100%\r'
@@ -81,4 +91,69 @@ func_progressbar()
     sleep 0.03
     done
     echo -e "\e[m"
+    return 0
+}
+func_uninstall()
+{
+  func_term_output
+  while ((!gueltig)); do    # beginn der Ja/Nein Abfrage
+    echo -e "\033[1;34m"
+    read -sn1 -t 30 -p "Möchtest Du den Calibre-Autoupdater wirklich DeInstallieren?  [j/n] " answer
+    echo -e "\e[m"
+    case "$answer" in
+	[JjYy])    result=1; gueltig=1 ;;
+	[Nn])      result=0; gueltig=1 ;;
+	"")        result=0; gueltig=1 ;;
+	*)         gueltig=0 ;;
+    esac
+  done
+  echo
+    if (( ! result )); then
+	echo -e "\033[1;31mDie DeInstallation des Calibre-Autoupdaters wurde durch Dich abgebrochen.\e[m\n"
+	exit 1  
+    fi
+    echo -e "\033[1;31mDer Calibre-Autoupdater wird nun deinstalliert.\e[m"
+    if [ -w "$CALIBRE_INSTALL_LOCATION" ]; then
+	rm -rf $INC"calibre-autoupdate.h" $INC"calibre-autoupdate_checks.h"; >&2
+	rm -rf $ETC"calibre-autoupdate.conf"; >&2
+	rm -rf $BIN"calibre-autoupdate"; >&2
+    else	     	     
+	sudo rm -rf $INC"calibre-autoupdate.h" $INC"calibre-autoupdate_checks.h"; >&2
+	sudo rm -rf $ETC"calibre-autoupdate.conf"; >&2
+	sudo rm -rf $BIN"calibre-autoupdate"; >&2
+    fi   
+    func_progressbar
+    echo -e "\n\033[1;32mDer Calibre-Autoupdater wurde deinstalliert. Schade das Du ihn nicht weiter verwendest\n\e[m"
+}
+func_uninstall_calibre()
+{
+  func_term_output
+  if test ! -d $CALIBRE_INSTALL_LOCATION/calibre ; then
+    echo -e "\033[1;31mEs konnte keine Calibre Installation gefunden werden. Die DeInstallation wurde abgebrochen\e[m\n"
+    exit 1
+  fi
+  while ((!gueltig)); do    # beginn der Ja/Nein Abfrage
+    echo -e "\033[1;34mMöchtest Du Calibre wirklich DeInstallieren? Calibre kann dann nicht mehr verwendet werden!"
+    read -sn1 -t 30 -p "Deine Bücher und die Calibre Bibliothek bleiben erhalten.  [j/n] " answer
+    echo -e "\e[m"
+    case "$answer" in
+	[JjYy])    result=1; gueltig=1 ;;
+	[Nn])      result=0; gueltig=1 ;;
+	"")        result=0; gueltig=1 ;;
+	*)         gueltig=0 ;;
+    esac
+  done
+  echo
+    if (( ! result )); then
+	echo -e "\n\033[1;31mDie DeInstallation von Calibre wurde durch Dich abgebrochen.\e[m\n"
+	exit 1  
+    fi
+    echo -e "\033[1;34mCalibre wird nun deinstalliert.\e[m"    
+    if [ -w "$CALIBRE_INSTALL_LOCATION" ]; then
+	calibre-uninstall
+    else	     	     
+	sudo calibre-uninstall
+    fi
+  echo -e "\n\n\033[1;32mCalibre würde DeInstalliert! Schade es ist ein tolles Programm zur eBookverwaltung :-)\e[m"
+  return 0
 }
--- calibre-autoupdate_checks.h
+++ calibre-autoupdate_checks.h
@@ -3,21 +3,21 @@
 #
 
 func_http_status_code () {
-  echo -e "\033[1;34m Hole Status Code von $CHECK_CALIBRE_DOWNLOAD_PAGE. Bitte warten.";
+  echo -e "\033[1;34m Hole Status Code von $CHECK_CALIBRE_DOWNLOAD_PAGE. Bitte warten.\e[m"
   stat_1=$(curl -o /dev/null --silent --head --write-out '%{http_code}' $CHECK_CALIBRE_DOWNLOAD_PAGE)
   func_progressbar
-  echo -e "\033[32m Status Codes von $CHECK_CALIBRE_DOWNLOAD_PAGE erhalten";
-  echo -e "\n\033[1;34m Hole Status Code von $CHECK_DOWNLOAD_URL. Bitte warten.";
+  echo -e "\033[32m Status Codes von $CHECK_CALIBRE_DOWNLOAD_PAGE erhalten\e[m"
+  echo -e "\n\033[1;34m Hole Status Code von $CHECK_DOWNLOAD_URL. Bitte warten.\e[m"
   stat_2=$(curl -o /dev/null --silent --head --write-out '%{http_code}' $CHECK_DOWNLOAD_URL)
   func_progressbar
-  echo -e "\033[32m Status Codes von $CHECK_DOWNLOAD_URL erhalten";
+  echo -e "\033[32m Status Codes von $CHECK_DOWNLOAD_URL erhalten\e[m"
   return 0
 }
 
 func_check_stat () {          # Funktion zum Check Verfügbarkeit der Downloadseiten und der Internetverbindung
 # Test for network conection
-echo -e "\n\033[1;34m Ich schau dann mal ob Dein Computer überhaupt ein Netzwerk hat :-)";
-for INTERFACE in $(ls /sys/class/net/ | grep -v lo);
+echo -e "\n\033[1;34m Suche nach vorhandenen Netzwerk für die Verbindung zum Internet :-)\e[m"
+for INTERFACE in $(ls /sys/class/net/ | grep -v lo)
 do
   if [[ $(cat /sys/class/net/$INTERFACE/carrier) = 1 ]]; then 
    ONLINE=1
@@ -25,8 +25,8 @@ do
 done
 func_progressbar
 if [ $ONLINE ]; then
-      echo -e "\n\033[32m Oh Wunder! Habe ein Netzwerk gefunden. Verbinde über Interface $INTERFACE,
- was nicht heißt das es geht :-P\n";
+      echo -e "\033[32m Es wurde ein Netzwerkinterface gefunden. Verbinde über Interface $INTERFACE\n\e[m"
+      func_http_status_code
       if [[ $stat_1 -eq 200 && $stat_2 -eq 200 ]]; then
 	  return 1
       elif [[ $stat_1 -eq !200 ]]; then
@@ -35,7 +35,7 @@ if [ $ONLINE ]; then
 	  return 3
       fi
 else
-    echo -e "\n\033[31m Fehler!!! Bitte schau Dir die Desktop Benachrichtigung an!";
+    echo -e "\n\033[31m Fehler!!! Bitte schau Dir die Desktop Benachrichtigung an!\e[m"
     return 4
 fi
 }
@@ -43,14 +43,14 @@ fi
 func_check_run_calibre () {
   CALIBRE_PID=`ps ax | grep /opt/calibre/bin/calibre | grep -v grep | awk '{printf $1}'`
 
-  while [ $CALIBRE_PID ]; 
+  while [ $CALIBRE_PID ]
   do
     $NOTIFY "Um das Update installieren zu können, muss Calibre beendet werden. Calibre wird in einer Minute vom Update Service beendet. !!!Bitte speichere alle wichtigen Daten!!!"
     sleep 3
     echo ""
-    for (( i=60; i>0; i-- ));
+    for (( i=60; i>0; i-- ))
     do
-      echo -e -n "\033[31m noch \033[32m>>$i<< \033[31m Sekunden bis zum Calibre Programmende\r"
+      echo -e -n "\033[31m noch \033[32m>>$i<< \033[31m Sekunden bis zum Calibre Programmende\r\e[m"
       sleep 1
     done
     kill -15 $CALIBRE_PID
@@ -71,21 +71,21 @@ func_check_version () {
 func_check_prog () {
   func_term_output  
   if test ! -f $CURL ; then
-    echo -e "\033[31mDu hast Curl nich installiert, es wird aber für die Updatefunktion benötigt\n"; >&2
+    $NOTIFY "Du hast Curl nich installiert, es wird aber für die Updatefunktion benötigt"
     exit 1
   fi
   
   if test ! -f $AWK ; then
-    echo -e "\033[31mDu hast AWK nich installiert, es wird aber für die Updatefunktion benötigt\n"; >&2
+    $NOTIFY "Du hast AWK nich installiert, es wird aber für die Updatefunktion benötigt"
     exit 1
   
   if test ! -f $NOTIFY ; then
-    echo -e "\e[0;33mDu hast Notify-Send nich installiert, das Programm ist optional und wird"; >&2
-    echo -e "für die Upatefunktion nicht unbedingt benötigt\n"; >&2
+    echo -e "\e[0;33mDu hast Notify-Send nich installiert, das Programm ist optional und wird\e[m"
+    echo -e "für die Upatefunktion nicht unbedingt benötigt\n\e[m"
     sleep 10
   fi
   
   fi  
     echo -e "\033[32mAlle benötigten Programme sind installiert. Starte $PROGNAME ohne Optionen und"
-    echo -e "es wird nach einer aktuellen Calibre Version geschaut\n";
+    echo -e "es wird nach einer aktuellen Calibre Version gesucht\n\e[m";
 }
